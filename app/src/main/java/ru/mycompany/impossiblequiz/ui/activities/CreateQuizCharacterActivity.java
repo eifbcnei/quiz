809823be6dd8a5457b1a5cc315852d91c628a086
eifbcnei <maxim99nn@gmail.com>
2019-08-28 16:15:37 +0300

    added android annotations into activity classes

--- app/src/main/java/ru/mycompany/impossiblequiz/ui/activities/CreateQuizCharacterActivity.java
+++ app/src/main/java/ru/mycompany/impossiblequiz/ui/activities/CreateQuizCharacterActivity.java
@@ -1,14 +1,10 @@
 package ru.mycompany.impossiblequiz.ui.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -16,6 +12,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OnActivityResult;
+import org.androidannotations.annotations.ViewById;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,108 +34,97 @@ import ru.mycompany.impossiblequiz.ui.custom.CircleImageView;
 import ru.mycompany.impossiblequiz.utils.Validation;
 import ru.mycompany.impossiblequiz.viewmodels.CreatorViewModel;
 
+@EActivity(R.layout.activity_create_quiz_character)
 public class CreateQuizCharacterActivity extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 1;
     private static final int IMAGE_NOT_CHOSEN = Color.RED;
 
     private CreatorViewModel viewModel;
-    private ListView questionsList;
-    private FloatingActionButton saveBtn;
-    private CircleImageView avatarCiv;
+    @ViewById(R.id.lv_questions_input)
+    ListView questionsList;
+    @ViewById(R.id.fab_save_character)
+    FloatingActionButton saveBtn;
+    @ViewById(R.id.civ_avatar_picker)
+    CircleImageView avatarCiv;
+    @ViewById(R.id.et_name)
+    EditText nameInput;
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        // Result code is RESULT_OK only if the user selects an Image
-        if (resultCode == Activity.RESULT_OK)
-            switch (requestCode) {
-                case PICK_IMAGE:
-                    //data.getData returns the content URI for the selected Image
-                    Uri selectedImage = data.getData();
-                    Drawable defaultAvatar = getDrawable(R.drawable.ic_default_avatar_150dp);
-                    viewModel.onAvatarChanged(selectedImage, defaultAvatar);
-                    break;
-            }
+    @OnActivityResult(PICK_IMAGE)
+    void onAvatarSelected(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Drawable defaultAvatar = getDrawable(R.drawable.ic_default_avatar_150dp);
+            //should contain not default image
+            viewModel.onAvatarChanged(selectedImage, defaultAvatar);
+        }
     }
 
-    private void initViewModel() {
+
+
+    @AfterViews
+    void initViewModel() {
         viewModel = ViewModelProviders.of(this).get(CreatorViewModel.class);
         viewModel.onInit(avatarCiv);
     }
 
-    private ListAdapter createInputAdapter(int elementsCount) {
-        List<QuestionBuilder> list = new ArrayList<>(elementsCount);
-        for (int i = 0; i < elementsCount; i++) {
+    @AfterViews
+    void setAvatarNotChosen() {
+        avatarCiv.setStrokeColor(IMAGE_NOT_CHOSEN);
+    }
+
+    @AfterViews
+    void createInputAdapter() {
+        Intent intent = getIntent();
+        int questionsCount = intent.getIntExtra("QUESTIONS_COUNT", 3);
+
+        List<QuestionBuilder> list = new ArrayList<>(questionsCount);
+        for (int i = 0; i < questionsCount; i++) {
             list.add(new QuestionBuilder());
         }
 
-        return new QuestionAdapter(this, R.layout.question_item, list);
+        questionsList.setAdapter(new QuestionAdapter(this, R.layout.question_item, list));
     }
 
-    private void initViews() {
-        avatarCiv = findViewById(R.id.civ_avatar_picker);
-        avatarCiv.setStrokeColor(IMAGE_NOT_CHOSEN);
-        saveBtn = findViewById(R.id.fab_save_character);
-        questionsList = findViewById(R.id.lv_questions_input);
+    @Click(R.id.fab_save_character)
+    void saveQuizCharacter() {
+        List<Question> usersInput;
+        QuizCharacter quizCharacter;
+        try {
+            checkAvatar();
+            QuestionAdapter adapter = (QuestionAdapter) questionsList.getAdapter();
+            usersInput = adapter.getUsersInput();
+            String name = getNameInput();
+
+            quizCharacter = new QuizCharacterBuilder()
+                    .setName(name)
+                    .setQuestions(usersInput)
+                    .setAvatarUri(viewModel.getAvatarUri())
+                    .createQuizCharacter();
+
+            returnActivityResult(quizCharacter);
+        } catch (ValidationException e) {
+            switch (e.getCode()) {
+                case INVALID_NAME:
+                    showWarning("invalid name");
+                    break;
+                case INVALID_AVATAR:
+                    showWarning("invalid avatar");
+                    break;
+                case INVALID_QUESTION:
+                    showWarning("invalid question");
+                    break;
+            }
+        }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_quiz_character);
-
-        initViews();
-        initViewModel();
-
-        Intent intent = getIntent();
-
-        int questionsCount = intent.getIntExtra("QUESTIONS_COUNT", 3);
-
-
-        questionsList.setAdapter(createInputAdapter(questionsCount));
-
-        avatarCiv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setType("image/*");
-                String[] mimeTypes = {"image/jpeg", "image/png"};
-                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
-                startActivityForResult(intent, PICK_IMAGE);
-            }
-        });
-
-        saveBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                List<Question> usersInput;
-                QuizCharacter quizCharacter;
-                try {
-                    checkAvatar();
-                    QuestionAdapter adapter = (QuestionAdapter) questionsList.getAdapter();
-                    usersInput = adapter.getUsersInput();
-                    String name = getNameInput();
-                    quizCharacter = new QuizCharacterBuilder()
-                            .setName(name)
-                            .setQuestions(usersInput)
-                            .setAvatarUri(viewModel.getAvatarUri())
-                            .createQuizCharacter();
-                    returnActivityResult(quizCharacter);
-                } catch (ValidationException e) {
-                    switch (e.getCode()) {
-                        case INVALID_NAME:
-                            showWarning("invalid name");
-                            break;
-                        case INVALID_AVATAR:
-                            showWarning("invalid avatar");
-                            break;
-                        case INVALID_QUESTION:
-                            showWarning("invalid question");
-                            break;
-                    }
-                }
-            }
-        });
+    @Click(R.id.civ_avatar_picker)
+    void selectAvatar() {
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        String[] mimeTypes = {"image/jpeg", "image/png"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+        startActivityForResult(intent, PICK_IMAGE);
     }
 
     private void returnActivityResult(QuizCharacter quizCharacter) {
@@ -149,7 +140,6 @@ public class CreateQuizCharacterActivity extends AppCompatActivity {
     }
 
     private String getNameInput() throws ValidationException {
-        EditText nameInput = findViewById(R.id.et_name);
         String input = nameInput.getText().toString();
         if (Validation.isStringBlank(input))
             throw new ValidationException(ExceptionCodes.INVALID_NAME);
--- app/src/main/java/ru/mycompany/impossiblequiz/ui/activities/QuizActivity.java
+++ app/src/main/java/ru/mycompany/impossiblequiz/ui/activities/QuizActivity.java
@@ -1,23 +1,25 @@
 package ru.mycompany.impossiblequiz.ui.activities;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OnActivityResult;
+import org.androidannotations.annotations.OptionsItem;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
 
 import ru.mycompany.impossiblequiz.R;
 import ru.mycompany.impossiblequiz.models.QuizCharacter;
@@ -25,32 +27,30 @@ import ru.mycompany.impossiblequiz.ui.fragments.QuestionCountPickerFragment;
 import ru.mycompany.impossiblequiz.utils.Validation;
 import ru.mycompany.impossiblequiz.viewmodels.QuizViewModel;
 
+@EActivity(R.layout.activity_quiz)
+@OptionsMenu(R.menu.main_menu)
 public class QuizActivity extends AppCompatActivity implements QuestionCountPickerFragment.InputListener {
     private static final int CREATE_QC = 1;
     private QuizViewModel characterModel;
-    private ImageView characterView;
-    private TextView questionView;
-    private ImageButton checkBtn;
-    private EditText inputView;
+    @ViewById(R.id.iv_character)
+    ImageView characterView;
+    @ViewById(R.id.tv_question)
+    TextView questionView;
+    @ViewById(R.id.ib_check)
+    ImageButton checkBtn;
+    @ViewById(R.id.et_answer_input)
+    EditText inputView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
-        initViews();
-        initViewModel();
 
-        checkBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String answer = inputView.getText().toString();
-                inputView.setText("");
-                characterModel.onCheckAnswer(answer);
-            }
-        });
+    @Click(R.id.ib_check)
+    void onCheckAnswer() {
+        String answer = inputView.getText().toString();
+        inputView.setText("");
+        characterModel.onCheckAnswer(answer);
     }
 
-    public void initViewModel() {
+    @AfterViews
+    void initViewModel() {
         characterModel = ViewModelProviders.of(this).get(QuizViewModel.class);
 
         characterModel.getQuizCharacterLiveData().observe(this, new Observer<QuizCharacter>() {
@@ -61,6 +61,7 @@ public class QuizActivity extends AppCompatActivity implements QuestionCountPick
         });
     }
 
+
     private void updateQuiz(QuizCharacter quizCharacter) {
         questionView.setText(quizCharacter.getCurrentQuestion());
         characterView.setImageURI(characterModel.getQuizCharacterLiveData().getValue().getAvatarUri());
@@ -69,52 +70,29 @@ public class QuizActivity extends AppCompatActivity implements QuestionCountPick
         characterView.setColorFilter(Color.rgb(curStatus.red, curStatus.green, curStatus.blue), PorterDuff.Mode.MULTIPLY);
     }
 
-    private void initViews() {
-        characterView = findViewById(R.id.iv_character);
-        questionView = findViewById(R.id.tv_question);
-        inputView = findViewById(R.id.et_answer);
-        checkBtn = findViewById(R.id.ib_check);
+    @OptionsItem(R.id.action_create_character)
+    void createQC() {
+        QuestionCountPickerFragment fr = new QuestionCountPickerFragment();
+        fr.show(getSupportFragmentManager(), "QuestionNumberDialog");
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
+    @OptionsItem(R.id.action_about)
+    void aboutApp() {
+        //TODO
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int itemId = item.getItemId();
-        switch (itemId) {
-            case R.id.create_character:
-                QuestionCountPickerFragment fr = new QuestionCountPickerFragment();
-                fr.show(getSupportFragmentManager(), "QuestionNumberDialog");
-                break;
-            case R.id.action_about:
-                //TODO
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (resultCode == Activity.RESULT_OK) {
-            switch (requestCode) {
-                case CREATE_QC:
-                    QuizCharacter newQC = data.getParcelableExtra(QuizCharacter.class.getSimpleName());
-                    characterModel.onNewQuizCharacterSelected(newQC);
-                    break;
-            }
+    @OnActivityResult(CREATE_QC)
+    void onResult(int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            QuizCharacter newQC = data.getParcelableExtra(QuizCharacter.class.getSimpleName());
+            characterModel.onNewQuizCharacterSelected(newQC);
         }
     }
 
     @Override
     public void onStartCreateActivity(int questionCount) {
         if (Validation.isQuestionCountValid(questionCount)) {
-            Intent createActivity = new Intent(this, CreateQuizCharacterActivity.class);
-            createActivity.putExtra("QUESTIONS_COUNT", questionCount);
-            startActivityForResult(createActivity, CREATE_QC);
+            CreateQuizCharacterActivity_.intent(this).extra("QUESTION_COUNT", questionCount).startForResult(CREATE_QC);
         } else {
             Toast.makeText(this, "try another count", Toast.LENGTH_LONG).show();
         }
