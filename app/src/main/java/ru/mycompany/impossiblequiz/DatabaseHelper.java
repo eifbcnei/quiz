746e49bc515b0e0f4cae2c21efff3847ae31478e
eifbcnei <maxim99nn@gmail.com>
2019-09-02 16:30:15 +0300

    reworked databaseImpl & added repository

--- app/src/main/java/ru/mycompany/impossiblequiz/DatabaseHelper.java
+++ app/src/main/java/ru/mycompany/impossiblequiz/DatabaseHelper.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,11 +15,13 @@ import ru.mycompany.impossiblequiz.models.QuizCharacter;
 import ru.mycompany.impossiblequiz.models.QuizCharacterBuilder;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
-    private final String COLUMN_ID = "_id";
-    private final String COLUMN_QUESTIONS = "QUESTIONS";
-    private final String COLUMN_ANSWERS = "ANSWERS";
-    private final String TABLE = "quizChars";
+    private static final String COLUMN_NAME = "NAME";
+    private static final String COLUMN_ID = "_id";
+    private static final String COLUMN_QUESTIONS = "QUESTIONS";
+    private static final String COLUMN_ANSWERS = "ANSWERS";
+    private static final String TABLE = "quizChars";
     private static final String DATABASE_NAME = "quizCharacters.db";
+    private static final String COLUMN_AVATAR_URI = "URI";
 
     private final String DIVIDER = "@";
 
@@ -47,20 +50,62 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = getReadableDatabase();
         Cursor query = db.rawQuery(String.format("SELECT * FROM " + TABLE + " WHERE " + COLUMN_ID + "=%s", Integer.toString(index)), null);
         query.moveToFirst();
-        List<String> questions = new ArrayList<>(Arrays.asList(query.getColumnName(1).split(DIVIDER)));
-        List<String> answers = new ArrayList<>(Arrays.asList(query.getColumnName(2).split(DIVIDER)));
+        String name = query.getColumnName(1);
+        List<String> questions = new ArrayList<>(Arrays.asList(query.getColumnName(2).split(DIVIDER)));
+        List<String> answers = new ArrayList<>(Arrays.asList(query.getColumnName(3).split(DIVIDER)));
         List<Question> questionList = new ArrayList<>();
+        Uri uri = Uri.parse(query.getColumnName(4));
         for (int i = 0; i != questions.size(); i++) {
             questionList.add(new Question(questions.get(i), answers.get(i)));
         }
-        QuizCharacter quizCharacter = new QuizCharacterBuilder().setQuestions(questionList).createQuizCharacter();
+        QuizCharacter quizCharacter = new QuizCharacterBuilder()
+                .setName(name)
+                .setQuestions(questionList)
+                .setAvatarUri(uri)
+                .createQuizCharacter();
         query.close();
         return quizCharacter;
     }
 
+    public List<QuizCharacter> getAllQuizCharacterData() {
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor query = db.rawQuery("SELECT * FROM " + TABLE, null);
+        List<QuizCharacter> quizCharacters = new ArrayList<>();
+        while (query.moveToNext()) {
+            String name = query.getColumnName(1);
+            List<String> questions = new ArrayList<>(Arrays.asList(query.getColumnName(2).split(DIVIDER)));
+            List<String> answers = new ArrayList<>(Arrays.asList(query.getColumnName(3).split(DIVIDER)));
+            List<Question> questionList = new ArrayList<>();
+            Uri uri = Uri.parse(query.getColumnName(4));
+            for (int i = 0; i != questions.size(); i++) {
+                questionList.add(new Question(questions.get(i), answers.get(i)));
+            }
+            QuizCharacter quizCharacter = new QuizCharacterBuilder()
+                    .setName(name)
+                    .setQuestions(questionList)
+                    .setAvatarUri(uri)
+                    .createQuizCharacter();
+            quizCharacters.add(quizCharacter);
+        }
+        query.close();
+        return quizCharacters;
+    }
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE " + TABLE + " (" + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," + COLUMN_QUESTIONS + " TEXT," + COLUMN_ANSWERS + " TEXT);");
+        db.execSQL("CREATE TABLE " + TABLE + " ("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_QUESTIONS + " TEXT,"
+                + COLUMN_ANSWERS + " TEXT,"
+                + COLUMN_AVATAR_URI + " TEXT);");
+
+        initDefaultQuizCharacters();
+    }
+
+    private void initDefaultQuizCharacters() {
+        SQLiteDatabase db = getWritableDatabase();
+        //TODO
     }
 
     @Override
