042ab83f5f3bbd9c3b1ad87501782bbd359a7b9c
eifbcnei <maxim99nn@gmail.com>
2019-09-02 16:29:19 +0300

    extracted some string resources

--- app/src/main/java/ru/mycompany/impossiblequiz/utils/Utils.java
+++ app/src/main/java/ru/mycompany/impossiblequiz/utils/Utils.java
@@ -1,9 +1,15 @@
 package ru.mycompany.impossiblequiz.utils;
 
+import android.content.res.Resources;
+
+import ru.mycompany.impossiblequiz.R;
+
 public class Utils {
     public static String getDifficulty(int questionCount) {
-        if (questionCount == 2 || questionCount == 3) return "Easy";
-        else if (questionCount == 4 || questionCount == 5) return "Middle";
-        else return "Extreme";
+        if (questionCount == 2 || questionCount == 3)
+            return Resources.getSystem().getString(R.string.mode_easy);
+        else if (questionCount == 4 || questionCount == 5)
+            return Resources.getSystem().getString(R.string.mode_medium);
+        else return Resources.getSystem().getString(R.string.mode_extreme);
     }
 }
