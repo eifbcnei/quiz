b3433e6999a4e1ca0db7351f11cef8f98b340706
eifbcnei <maxim99nn@gmail.com>
2019-09-03 20:40:18 +0300

    added button to save image from quizCharacters

--- app/src/main/java/ru/mycompany/impossiblequiz/ui/adapters/QuizCharacterAdapter.java
+++ app/src/main/java/ru/mycompany/impossiblequiz/ui/adapters/QuizCharacterAdapter.java
@@ -1,5 +1,9 @@
 package ru.mycompany.impossiblequiz.ui.adapters;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,19 +11,23 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import ru.mycompany.impossiblequiz.App;
 import ru.mycompany.impossiblequiz.R;
 import ru.mycompany.impossiblequiz.models.QuizCharacter;
 import ru.mycompany.impossiblequiz.ui.custom.CircleImageView;
+import ru.mycompany.impossiblequiz.utils.ImageUtils;
 import ru.mycompany.impossiblequiz.utils.Utils;
 
 public class QuizCharacterAdapter extends RecyclerView.Adapter<QuizCharacterAdapter.ViewHolder> {
-    private List<QuizCharacter> quizCharacters=new ArrayList<>();
+    private List<QuizCharacter> quizCharacters = new ArrayList<>();
     private QuizCharacterSelector selector;
 
     public QuizCharacterAdapter(QuizCharacterSelector selector) {
@@ -38,12 +46,37 @@ public class QuizCharacterAdapter extends RecyclerView.Adapter<QuizCharacterAdap
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         final QuizCharacter qc = quizCharacters.get(position);
         holder.avatar_civ.setImageURI(qc.getAvatarUri());
-        holder.name_tv.setText(qc.getName());
+        holder.name_tv.setText(Long.toString(qc.getId()));
         holder.difficulty_tv.setText(Utils.getDifficulty(qc.getQuestionCount()));
         holder.select_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 selector.onItemQuizCharacterSelected(qc);
+            }
+        });
+        final Uri source = holder.avatar_civ.getmImageUri();
+        holder.save_image_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO(not working :D)
+                Bitmap bitmapImage = ImageUtils.getBitmap(ImageUtils.getDrawableFromUri(source));
+                ContextWrapper cw = new ContextWrapper(App.applicationContext());
+                File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+                File mypath = new File(directory, qc.getName().replaceAll(" ", "_") + ".jpg");
+
+                FileOutputStream fos = null;
+                try {
+                    fos = new FileOutputStream(mypath);
+                    bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
         });
     }
@@ -54,36 +87,13 @@ public class QuizCharacterAdapter extends RecyclerView.Adapter<QuizCharacterAdap
     }
 
     public void updateData(final List<QuizCharacter> newData) {
-        DiffUtil.Callback callback = new DiffUtil.Callback() {
-            @Override
-            public int getOldListSize() {
-                return quizCharacters.size();
-            }
-
-            @Override
-            public int getNewListSize() {
-                return newData.size();
-            }
-
-            @Override
-            public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
-                return quizCharacters.get(oldItemPosition) == newData.get(newItemPosition);
-            }
-
-            @Override
-            public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
-                return quizCharacters.get(oldItemPosition).equals(newData.get(newItemPosition));
-            }
-        };
-        DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(callback);
         quizCharacters = newData;
-        diffResult.dispatchUpdatesTo(this);
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
         final TextView name_tv, difficulty_tv;
         final CircleImageView avatar_civ;
-        final ImageButton select_btn;
+        final ImageButton select_btn, save_image_btn;
 
         ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -91,6 +101,7 @@ public class QuizCharacterAdapter extends RecyclerView.Adapter<QuizCharacterAdap
             difficulty_tv = itemView.findViewById(R.id.tv_difficulty_value);
             avatar_civ = itemView.findViewById(R.id.civ_qc_avatar);
             select_btn = itemView.findViewById(R.id.btn_select);
+            save_image_btn = itemView.findViewById(R.id.btn_save_image);
         }
     }
 }
